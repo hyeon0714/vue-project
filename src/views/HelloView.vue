7f183eade4126732f918657a282e955a9c43c513
hyeon0714 <xodid6960@naver.com>
2024-03-20 16:42:25 +0900

    뷰

---
+++ src/views/HelloView.vue
@@ -0,0 +1,20 @@
+<template> 
+    <h1>
+        헬로뷰 hellowview
+    </h1>
+</template><!--html 작성 영역--> 
+
+<script>
+    export default {
+        name: "HelloView",
+        
+        data(){
+            return{};
+        }
+    };
+
+</script>
+
+<style>
+
+</style>
